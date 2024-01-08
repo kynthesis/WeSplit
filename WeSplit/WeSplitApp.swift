5d78239db3cd15d02ad8379cca8988c2af817ab6
Khoa D. Vo <khoa.synthesis@gmail.com>
2024-01-08 21:41:59 +0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Khoa D. Vo on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
